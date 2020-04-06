38096dfb89a083b89b005ceb2dcfb37c3b76f048
tiger66866 <tiger66866@gmail.com>
2020-04-06 18:33:18 -0400

    added website

---
+++ cb.js
@@ -0,0 +1,31 @@
+const clientID = 'fims-062a4c23a50918e9f3cbc208a42e08647653895110161406010'
+const clientSecret = 'nbJ3NUYbQTkyuEagbl5oHe7j85FGlopLur4hrHCr'
+const oauth2BaseURL = 'https://api.kroger.com/v1/connect/oauth2'
+const apiBaseURL = 'https://api.kroger.com'
+const redirectURL = 'https://fimshub.github.io'
+
+function setup() {
+
+}
+
+function draw() {
+
+}
+
+async function getToken() {
+  var url = document.createElement('a');
+  url.href = window.location.href;
+  let authCode = url.search.split('=')[1];
+  const code = {
+    authCode
+  };
+  const payload = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(code)
+  };
+  const response = await fetch('http://69.142.173.103:3001/incomingAuthCode', payload);
+  const data = await response.json();
+}
---
+++ main.js
@@ -0,0 +1,47 @@
+const clientID = 'fims-062a4c23a50918e9f3cbc208a42e08647653895110161406010'
+const clientSecret = 'nbJ3NUYbQTkyuEagbl5oHe7j85FGlopLur4hrHCr'
+const oauth2BaseURL = 'https://api.kroger.com/v1/connect/oauth2'
+const apiBaseURL = 'https://api.kroger.com'
+const redirectURL = 'https://fimshub.github.io'
+
+let authToken = null;
+let refToken = null;
+
+function setup() {
+  setToken();
+}
+
+function draw() {
+
+}
+
+function setToken() {
+  const send = {};
+  const payload = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(send)
+  };
+  fetch('http://69.142.173.103:3001/getAuthToken', payload).then(response => {
+    response.json().then(data => {
+      authToken = data.authToken;
+      refToken = data.refToken;
+    });
+  });
+  if (refToken == null) {
+    redirectToLogin();
+  }
+}
+
+function redirectToLogin() {
+  const scope = encodeURIComponent('cart.basic:write product.compact');
+  const url =
+    `${oauth2BaseURL}/authorize?` +
+    `client_id=${encodeURIComponent(clientID)}` +
+    `&redirect_uri=${encodeURIComponent(redirectURL)}` +
+    `&response_type=code` +
+    `&scope=${scope}`;
+  window.location = url;
+}
